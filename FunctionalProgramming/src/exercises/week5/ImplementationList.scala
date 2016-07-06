1209eca2773674f517f48e2a5199fe4fab5028d3
manuPerez <perezdelgadomanu@gmail.com>
2016-07-06 20:56:47 +0200

    Week 5. Pairs and Tuples.

--- FunctionalProgramming/src/exercises/week5/ImplementationList.scala
+++ FunctionalProgramming/src/exercises/week5/ImplementationList.scala
@@ -2,7 +2,7 @@ package week5
 
 import scala.collection.mutable.ListBuffer
 
-class ImplementationList {
+object ImplementationList extends App{
 
   def Last[T](list: List[T]): T = {
     list match {
@@ -15,7 +15,7 @@ class ImplementationList {
   def init[T](list: List[T]): List[T] = {
     list match {
       case List() => throw new Exception("Emty list")
-      case List(x) => _
+      case List(x) => List()
       case x :: xs => x :: init(xs)
     }
   }
@@ -34,4 +34,29 @@ class ImplementationList {
   }
 
   def removeAt[T](x: Int, list: List[T]): List[T] = list.take(x) ::: list.drop(x + 1)
+
+  def msort(xs: List[Int]): List[Int] = {
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      def merge(xs: List[Int], ys: List[Int]): List[Int] =
+        (xs, ys) match {
+          case (Nil, y1 :: ys1) =>
+            ys
+          case (x1 :: xs1, Nil) =>
+            xs
+          case (x2 :: xs2, y2 :: ys2) =>
+            if (x2 < y2) x2 :: merge(xs2, ys)
+            else y2 :: merge(xs, ys2)
+        }
+      val (fst, snd) = xs splitAt n
+      merge(msort(fst), msort(snd))
+    }
+  }
+
+  val nums = List(2, -4, 5, 7, 1)
+  val numsOrdered = msort(nums)
+
+  println(numsOrdered)
+
 }
